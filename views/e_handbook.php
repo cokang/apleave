fd2fc224f00c22e66fc75d854014b36575ade966
faiz <muhammadfaiznoh@gmail.com>
2020-07-03 10:59:41 +0800

    Update e_handbook.php
    added print button

--- views/e_handbook.php
+++ views/e_handbook.php
@@ -2,7 +2,14 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url();?>js/pdfjs/css/style.css">
 <script src="<?=base_url();?>js/pdfjs/build/pdf.js"></script>
 <script src="<?=base_url();?>js/pdfjs/viewer.js"></script>
+<script Language="javascript">
 
+function printfile(id) {
+    window.frames[id].focus();
+    window.frames[id].print();
+}
+
+</script>
 
 
 <div id="page-wrapper">
@@ -22,9 +29,12 @@
 				<div class="panel-body">
 					<div class="row">
 						<div class="col-lg-12">
-							<iframe src='<?=base_url()."js/pdfjs/web/viewer.html?file=".base_url()."buletin_file/$file_name";?>' id="pdfviewer"></iframe>
+							<iframe src='<?=base_url()."js/pdfjs/web/viewer.html?file=".base_url()."buletin_file/APP.pdf";?>' id="pdfviewer"></iframe>
 						</div>
 					</div>
+					<iframe hidden src="/app.pdf" id="objAdobePrint" name="objAdobePrint" height="95%" width="100%" frameborder=0></iframe><br>
+					<button type="button" class="btn btn-default" value="Print" onclick="javascript:printfile('objAdobePrint');">Print Acknowledgment of Acceptance
+</button>
 				</div>
 			</div>
 		</div>
