7b9094d85b662432a91aa5796cbadfe5c1a16099
faiz <muhammadfaiznoh@gmail.com>
2020-07-03 11:06:50 +0800

    Create e_handbook.php

---
+++ views/e_handbook.php
@@ -0,0 +1,42 @@
+<link rel="resource" type="application/l10n" href="<?=base_url();?>js/pdfjs/web/locale/locale.properties">
+<link rel="stylesheet" type="text/css" href="<?=base_url();?>js/pdfjs/css/style.css">
+<script src="<?=base_url();?>js/pdfjs/build/pdf.js"></script>
+<script src="<?=base_url();?>js/pdfjs/viewer.js"></script>
+<script Language="javascript">
+
+function printfile(id) {
+    window.frames[id].focus();
+    window.frames[id].print();
+}
+
+</script>
+
+
+<div id="page-wrapper">
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">Staff Handbook</h1>
+		</div>
+
+
+	</div>
+
+
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+				<div class="panel-heading"> Staff Handbook </div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-lg-12">
+							<iframe src='<?=base_url()."js/pdfjs/web/viewer.html?file=".base_url()."buletin_file/APP.pdf";?>' id="pdfviewer"></iframe>
+						</div>
+					</div>
+					<iframe hidden src="/app.pdf" id="objAdobePrint" name="objAdobePrint" height="95%" width="100%" frameborder=0></iframe><br>
+					<button type="button" class="btn btn-default" value="Print" onclick="javascript:printfile('objAdobePrint');">Print Acknowledgment of Acceptance
+</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
